a3af02cd168122790d1a22f64f898d3cb5bbf5fa
Ayaka Yoshizawa <a-chan.177002@outlook.com>
2020-04-13 22:36:24 +0900

    HealthApplication コメント追加

--- app/src/main/java/com/a_rin/health/HealthApplication.kt
+++ app/src/main/java/com/a_rin/health/HealthApplication.kt
@@ -4,6 +4,13 @@ import android.app.Application
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
+/*
+ * タイトル : HealthApplication
+ * 説明 : KibunChanのアプリケーションクラス
+ *
+ * @author Ayaka Yoshizawa
+ */
+
 class HealthApplication : Application() {
 
     override fun onCreate() {
