e018b4b85163640cd5c472be5143c324bff330c2
Ayaka Yoshizawa <a-chan.177002@outlook.com>
2020-04-13 22:37:29 +0900

    ItemData コメント追加

--- app/src/main/java/com/a_rin/health/ItemData.kt
+++ app/src/main/java/com/a_rin/health/ItemData.kt
@@ -4,6 +4,13 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
+/*
+ * タイトル : ItemData
+ * 説明 : 気分記録を格納するモデル
+ *
+ * @author Ayaka Yoshizawa
+ */
+
 open class ItemData(
     @PrimaryKey open var date: String ?= null,
         open var year : Int ?= null,
